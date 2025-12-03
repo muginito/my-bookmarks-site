3cdf2eaab78c12f9da934b298cb00a9ed537cee5
muginito <miguel.vramos04@gmail.com>
2025-12-03 17:03:52 -0300

    Começa a implmentar React
    Adiciona alguns componentes, import de estilos, move estilos para
    /src/styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import './App.css'
+import InputBar from "./components/InputBar.jsx"
+import Bookmark from "./components/Bookmark.jsx"
+// import Header from './components/Header.jsx'
 
-
-function BookmarkForm() {
-
-}
-
-function BookmarkItem() {
-
-}
-
-function BookmarkList({ bookmarks }) {
-  const bookmarksItems = []
-
-  return (
-
-  )
-}
-
-function Header() {
-  return (
-    <header>
-      <h1>My Bookmarks</h1>
-      <InputBar type="text" placeholder="Paste URL" />
-      <InputBar type="search" placeholder="Search bookmarks..." />
-    </header>
-  )
-}
-
-function InputBar({ type, placeholder }) {
-  return
-}
+// function BookmarkForm() {
+//
+// }
+//
+// function BookmarkItem() {
+//
+// }
+//
+// function BookmarkList({ bookmarks }) {
+//   const bookmarksItems = []
+//
+//   return (
+//
+//   )
+// }
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="m-auto flex w-[75%] max-w-[900px] flex-col items-center justify-center">
+        <h1 className="font-heading my-12 w-screen text-center text-4xl font-bold text-yellow-400 md:my-24 md:text-5xl">
+          My Bookmarks
+        </h1>
+        <div className="flex w-full flex-col gap-4">
+          <InputBar id="url" type="text" placeholder="Paste URL" />
+          <InputBar id="search" type="search" placeholder="Search..." />
+        </div>
+        <hr />
+      </header>
+
+      <section className="flex w-full flex-col items-center">
+        <Bookmark />
+        <Bookmark />
+        <Bookmark />
+      </section>
     </>
   )
 }
