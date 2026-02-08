bdbbe464aaf08f693e7cc564a9bbfc48ba5beb70
Igor Matta <nelofanz@gmail.com>
2026-02-08 18:02:09 -0300

    Mecanismo de pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,25 @@ import BookmarkList from "./components/BookmarkList.jsx"
 import BookmarkForm from "./components/BookmarkForm.jsx"
 import DeletionPopup from "./components/DeletionPopup.jsx"
 import Header from "./components/Header.jsx"
+import Fuse from 'fuse.js'
 
 function App() {
   const [isFormOpen, setIsFormOpen] = useState(false)
   const [bookmarks, setBookmarks] = useLocalStorage("bookmarks", [])
   const [selectedBookmark, setSelectedBookmark] = useState(null)
   const [delConfirm, setDelConfirm] = useState({ show: false, id: null })
+
+ // configuração do Fuse.js (busca)
+  const [busca, setBusca] = useState("")
+  const opcoesFuse = {
+    keys: ["title", "author", "description", "url", "year"],
+    threshold: 0.2, //sensibilidade, coloquei no "padrão  "
+  };
+  const fuse = new Fuse(bookmarks, opcoesFuse);
+  // logica de filtragem, se a lista tiver vazia ele mostra a lista toda
+  const resultadoBusca = busca
+    ? fuse.search(busca).map(res => res.item)
+    : bookmarks;
 
   // Open New Form
   function handleOpenNewForm() {
@@ -66,11 +79,13 @@ function App() {
   //   setBookmarks(delBookmark)
   // }
 
+// linha 82 <Header newForm={handleOpenNewForm} /> (edit)
+// linha 85 bookmarks={bookmarks} (edit)
   return (
     <>
-      <Header newForm={handleOpenNewForm} />
+      <Header newForm={handleOpenNewForm} onSearch={setBusca} />
       <BookmarkList
-        bookmarks={bookmarks}
+        bookmarks={resultadoBusca}
         onDelete={handleDeleteBookmark}
         onEdit={handleOpenEditForm}
       />
@@ -91,5 +106,7 @@ function App() {
     </>
   )
 }
+
+
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import InputBar from "./InputBar"
 
-export default function Header({ newForm }) {
+export default function Header({ newForm, onSearch }) {
+  // Coloquei a função de busca do App
   return (
     <header className="m-auto flex w-[75%] max-w-[900px] flex-col items-center justify-center">
       <h1 className="font-heading my-12 w-full text-center text-3xl font-bold text-yellow-400 md:my-24 md:text-5xl">
@@ -13,6 +14,8 @@ export default function Header({ newForm }) {
           type="search"
           placeholder="Search..."
           // className="text-sm md:text-base md:font-normal"
+          onChange={(e) => onSearch(e.target.value)}
+          // Depois que o usuario digita algo, é enviado para o app atualizar o estado
         />
         <button
           className="bg-dark-ye text-dark-bg-2 w-fit cursor-pointer rounded-4xl px-4 text-sm font-bold whitespace-nowrap hover:bg-yellow-300 active:scale-95 md:text-base"
