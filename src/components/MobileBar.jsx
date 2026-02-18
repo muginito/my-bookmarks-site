e7e2a72614300cdfc615fafd319279093aa19acd
Igor Nunes <nelofanz@gmail.com>
2026-02-18 18:52:10 -0300

    Barra inferior consertada + Animação

--- src/components/MobileBar.jsx
+++ src/components/MobileBar.jsx
@@ -1,12 +1,33 @@
 import { faFileExport, faFileImport, faSquarePlus } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useNavigate } from "react-router"
+import { useState } from "react"
+import { motion, useScroll, useMotionValueEvent } from "framer-motion"
 
 export default function MobileBar({ onExport, onImport }) {
+  // Comecei a fazer numa animação para a barra não aparecer em cima dos itens e nem no menu de pagina (logica)
   const navigate = useNavigate()
+  const [isHidden, setIsHidden] = useState(false)
+  const { scrollY } = useScroll()
 
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious()
+    // se o usuario descer mais que 50px a barra some, se subir ela aparece
+    if (latest > previous && latest > 50) {
+      setIsHidden(true)
+    } else {
+      setIsHidden(false)
+    }
+  })
+  // Aproveitar que a gente vai usar motion, vou transformar essa div em motion.div e passar as regras
   return (
-    <div
+    <motion.div
+      variants={{
+        visible: { y: 0 },
+        hidden: { y: "100%" }, //A barra vai pra fora da tela
+      }}
+      animate={isHidden ? "hidden" : "visible"}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
       className="md:hidden right-0 bottom-0 left-0 z-50 fixed flex justify-between items-center
         bg-[#1a1a1a] px-6 border-dark-ui-3 border-t h-16"
     >
@@ -40,6 +61,6 @@ export default function MobileBar({ onExport, onImport }) {
 
       {/* Coloquei um espaçador invisivel pro botão "+" ficar no centro */}
       <div className="w-12 pointer-events-none"></div>
-    </div>
+    </motion.div>
   )
 }
