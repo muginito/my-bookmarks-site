6e453d913d0e27b492c0a0c1a403fb2cda1db47d
muginito <miguel.vramos04@gmail.com>
2026-02-21 22:03:22 -0300

    feat: enhance DeletionPopup with motion effects and replace buttons with AnimatedButton

--- src/components/DeletionPopup.jsx
+++ src/components/DeletionPopup.jsx
@@ -1,6 +1,8 @@
 import { useLockBodyScroll } from "react-use"
 import { useEffect } from "react"
 import { useNavigate } from "react-router"
+import { motion, spring } from "framer-motion"
+import AnimatedButton from "./AnimatedButton"
 
 export default function DeletionPopup({ onConfirm, children }) {
   useLockBodyScroll()
@@ -22,31 +24,34 @@ export default function DeletionPopup({ onConfirm, children }) {
   }, [navigate])
 
   return (
-    <div className="top-0 z-50 fixed flex justify-center">
-      <div className="backdrop-brightness-90 w-screen h-screen" onClick={() => navigate("/")}></div>
-      <div
-        className="top-18 absolute flex flex-col justify-center items-center bg-dark-ui mx-auto p-4
+    <div className="top-0 z-50 fixed flex justify-center text-white">
+      <div className="backdrop-brightness-95 w-screen h-screen" onClick={() => navigate("/")}></div>
+      <motion.div
+        initial={{ y: -200, opacity: 0, transition: { duration: 0.2 } }}
+        exit={{ y: -200, opacity: 0, transition: { duration: 0.2 } }}
+        animate={{ y: 0, opacity: 1 }}
+        className="top-10 absolute flex flex-col justify-center items-center bg-dark-ui mx-auto p-4
           border-2 border-dark-ui-3 rounded-2xl max-w-xl text-center"
       >
         <h1 className="p-2 font-bold text-red-500 text-lg">Atenção!</h1>
         <p className="p-2 px-8">{children}</p>
         <div className="flex justify-around mt-4 w-full">
-          <button
-            className="bg-dark-bg-2 hover:bg-red-700 shadow px-12 py-2 rounded-2xl duration-200
-              ease-in-out cursor-pointer"
+          <AnimatedButton
+            className="bg-dark-bg-2 hover:bg-red-700 shadow px-12 py-2 rounded-2xl
+              hover:text-red-200 cursor-pointer"
             onClick={onConfirm}
           >
             Sim
-          </button>
-          <button
-            className="bg-dark-bg-2 hover:bg-dark-ui-2 shadow px-12 py-2 rounded-2xl duration-200
-              ease-in-out cursor-pointer"
+          </AnimatedButton>
+          <AnimatedButton
+            className="bg-dark-bg-2 hover:bg-dark-ui-2 shadow px-12 py-2 rounded-2xl
+              hover:text-base-200 cursor-pointer"
             onClick={() => navigate("/")}
           >
             Não
-          </button>
+          </AnimatedButton>
         </div>
-      </div>
+      </motion.div>
     </div>
   )
 }
